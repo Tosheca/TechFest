cb6f6653723566057377b17ac2e5b1fe60c55228
Teo Pavlov <Teo@Teodors-MacBook-Pro.local>
2016-05-18 23:10:53 +0300

    Intializing project.

---
+++ components/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div id="app">
+	<h1>Hello {{name}}</h1>
+	<input v-model="name">
+</div>
+
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			name: "TechFest"
+		}
+	}
+}
+</script>
+
+<style>
+</style>
