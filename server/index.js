75a967ec50dd85f21db640ec3b9a7108a34a0486
Ivan Todorov <IvanTodorov@Ivans-MacBook-Pro.local>
2016-05-19 15:24:18 +0300

    Added registration.

--- server/index.js
+++ server/index.js
@@ -1,49 +1,50 @@
 var mongoose = require("mongoose")
+
 var koa = require("koa")
+var bodyParser = require('koa-bodyparser');
 var router = require('koa-router')();
 var serve = require('koa-static');
+
+const crypto = require('crypto');
 
 mongoose.connect('mongodb://localhost/test');
 
 var app = koa()
 
 var User = mongoose.model("User", {
-	name: String, password: String
+	name: { type: String, index: { unique: true }, required: true },
+	password: { type: String, required: true }
 })
 
 router.post('/api/register', function *() {
-	var a = new User({
-		name: "Ivan",
-		password: "123"
+	let {username, password} = this.request.body
+
+	let user = new User({
+		name: username,
+		password: crypto.createHash('sha256').update(password).digest("base64")
 	})
 
-	a.save()
+	try {
+		yield user.save()
+		this.body = "Success"
+	}
+	catch(error) {
+		this.body = error.message
+	}
+})
 
-	var b = new User({
-		name: "Aleks",
-		password: "1234"
-	})
-
-	b.save()
-
-	var c = new User({
-		name: "Teo",
-		password: "12345"
-	})
-
-	c.save()
-
-	this.body = "Hello world"
+router.post('/', function *() {
+	let {username, password} = this.request.body
 })
 
 router.get('/api/hello', function *() {
+	User.find().remove().exec()
+
 	this.body = "Hello world"
 })
 
-
-
+app.use(bodyParser());
 app.use(router.routes())
-
 app.use(serve("../static/out"))
 
 app.listen(3000)
