17c70cb22a81f9190bb6a910050652012aa614a0
alex4o <mrabonin@gmail.com>
2016-05-23 12:59:43 +0300

    Graph drawing and such

--- static/components/CurrentProgram.vue
+++ static/components/CurrentProgram.vue
@@ -7,12 +7,36 @@
 	</div>
 </nav>
 
+<div id="network"  v-el:vis-container></div>
+
 <span>{{clicked}}</span>
 
 </template>
 
 
 <script>
+
+import vis from "vis"
+var options = {
+    nodes: {
+        scaling: {
+            min: 16,
+            max: 32
+        }
+    },
+    edges: {
+        color: "#777",
+        smooth: false
+    },
+    physics: {
+        barnesHut: {
+            gravitationalConstant: -30000
+        },
+        stabilization: {
+            iterations: 2500
+        }
+    }
+}
 
 export default {
 	methods: {  
@@ -25,6 +49,20 @@ export default {
 			let program = await this.$programs.getId(this.$route.params.id)
 			console.log(program)
 			next({ program })
+		},
+		async activate(){
+			window.n = new vis.Network(this.$els.visContainer, {} /* data */, options)
+			n.setData({ 
+				nodes: [
+					{id:1, value: 1},
+					{id:2, value: 12},
+					{id:3, value: 3}
+				],
+				edges: [
+					{id:1, from:1, to: 2},
+					{id:2, from:1, to: 3},
+					{id:3, from:2, to: 3}
+				]})
 		}
 	},
 	data() {
@@ -38,3 +76,16 @@ export default {
 </script>
 
 <style>
+	#network {
+		height: 80vh
+	}	
+
+	.vis-network {
+		height: 80vh
+
+	}
+
+	.vis-network canvas {
+		height: 80vh
+	}
+</style>
